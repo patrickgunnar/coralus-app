b68ffcd3d097364e112a541580dc19b07fc607c5
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-08 20:42:17 -0300

    feat: added content

--- src/components/client/Table.tsx
+++ src/components/client/Table.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { Component, ReactNode } from "react";
+import History from "../history/History";
+import CalculatorFrame from "../calculator/CalculatorFrame";
+import TVFrame from "../tv/TVFrame";
 
 
 class Table extends Component {
@@ -10,10 +13,10 @@ class Table extends Component {
 
     render(): ReactNode {
         return (
-            <div className="box-border flex justify-center items-center h-[90%] w-[95%] md:h-[70%] lg:h-[90%] lg:w-[55%] 
-            bg-customColor shadow-customA rounded-customA overflow-hidden">
-                Caralus
-            </div>
+            <TVFrame>
+                <History />
+                <CalculatorFrame />
+            </TVFrame>
         )
     }
 }
