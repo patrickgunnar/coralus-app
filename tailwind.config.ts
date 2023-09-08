1f722dae63fb13ca54e5a9a2ee89d219ac231261
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-08 20:17:06 -0300

    feat: added styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+				customLinearGradient: `linear-gradient(transparent 50%, var(--colour-004) 0),
+				conic-gradient(var(--degrees) 90%, var(--colour-002) 240deg, transparent 0),
+				conic-gradient(var(--degrees) 75%, var(--colour-001) 240deg, transparent 0),
+				conic-gradient(var(--degrees) 25%, transparent 240deg, var(--colour-002) 0),
+				conic-gradient(var(--degrees) 40%, var(--colour-002) 240deg, var(--colour-003) 0)`
+            },
+			backgroundColor: {
+				customColor: "var(--colour-007)"
+			},
+            backgroundSize: {
+                customSize: "calc(1.5 * var(--size)) var(--size)",
+            },
+			boxShadow: {
+				customA: "inset 17px 17px 34px var(--colour-005), inset -17px -17px 34px var(--colour-006)"
+			},
+			borderRadius: {
+				customA: "20px"
+			}
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
