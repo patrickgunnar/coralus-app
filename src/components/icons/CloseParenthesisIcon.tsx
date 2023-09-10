c283064d3af7bffbe7dc2f5723315eeacab16030
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-10 16:10:40 -0300

    feat: added custom icons

---
+++ src/components/icons/CloseParenthesisIcon.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const CloseParenthesisIcon = () => {
+    return (
+        <span className="text-[1rem] md:text-[1.5rem] font-medium font-sans"
+        aria-hidden="true">
+            )
+        </span>
+    );
+};
+
+export default CloseParenthesisIcon;
---
+++ src/components/icons/OpenParenthesisIcon.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const OpenParenthesisIcon = () => {
+    return (
+        <span className="text-[1rem] md:text-[1.5rem] font-medium font-sans"
+        aria-hidden="true">
+            (
+        </span>
+    );
+};
+
+export default OpenParenthesisIcon;
