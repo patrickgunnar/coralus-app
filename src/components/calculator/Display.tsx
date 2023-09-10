47732080fba34572be4ee2934032ca0caf3b4fd3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-10 16:11:25 -0300

    feat: display component

---
+++ src/components/calculator/Display.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import useExpression from "@/hooks/useExpression";
+import { Component, ReactNode } from "react";
+import { IconType } from "react-icons";
+import { PiDotLight, PiCaretUpBold } from "react-icons/pi";
+import { TbSquareRoot } from "react-icons/tb";
+
+interface DisplayProps {
+    expression: IconType[];
+    expString: string;
+}
+
+class Display extends Component<DisplayProps> {
+    constructor(props: DisplayProps) {
+        super(props);
+    }
+
+    render(): ReactNode {
+        const { expression } = this.props;
+
+        return (
+            <div
+                className="box-border flex flex-wrap gap-0 justify-end items-center h-[20%] w-full rounded-customA 
+            bg-customColor shadow-customH p-7 text-[1rem] md:text-[1.1rem] xl:text-[1.5rem] overflow-hidden"
+            >
+                {expression.map((Icon, index) => {
+                    if (Icon === PiDotLight) {
+                        return <PiDotLight key={index} className="mt-4" />;
+                    }
+
+                    if (Icon === PiCaretUpBold) {
+                        return <PiCaretUpBold key={index} className="mb-4" />;
+                    }
+
+                    if (Icon === TbSquareRoot) {
+                        return <TbSquareRoot key={index} className="mb-1" />;
+                    }
+
+                    return <Icon key={index} />;
+                })}
+            </div>
+        );
+    }
+}
+
+const DisplayWrapper = () => {
+    const { expression, expString } = useExpression();
+
+    return <Display expression={expression} expString={expString} />;
+};
+
+export default DisplayWrapper;
