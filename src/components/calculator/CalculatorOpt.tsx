fc0965cde9757919201ec55ea29551f2a1961646
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-10 16:10:22 -0300

    refactor: removed unnecessary wrapper

--- src/components/calculator/CalculatorOpt.tsx
+++ src/components/calculator/CalculatorOpt.tsx
@@ -1,33 +1,37 @@
-"user client"
+"user client";
 
 import { Component, ReactNode } from "react";
 import { IconType } from "react-icons";
 
 
-interface CalculatorOptProps {
-    icon: IconType
-    label: string
-    classNames: string
+interface CalculationWrapperProps {
+    icon: IconType;
+    label: string;
+    opt: string;
+    classNames: string;
+    onClickHandler: (opt: string, icon: IconType) => void;
 }
 
-class CalculatorOpt extends Component<CalculatorOptProps> {
-    constructor(props: CalculatorOptProps) {
+class CalculatorOpt extends Component<CalculationWrapperProps> {
+    constructor(props: CalculationWrapperProps) {
         super(props);
     }
 
     render(): ReactNode {
-        const { label, icon: Icon } = this.props
+        const { label, opt, icon: Icon, onClickHandler } = this.props;
+
         return (
             <div className="box-border flex justify-center items-center h-full w-full">
-                <button className={`box-border flex justify-center items-center h-full w-full 
-                rounded-full hover:opacity-75 ${this.props.classNames}`}>
-                    {
-                        !Icon ? label : <Icon size={25} />
-                    }
+                <button
+                    className={`box-border flex justify-center items-center h-full w-full 
+                rounded-full hover:opacity-75 ${this.props.classNames}`}
+                    onClick={() => onClickHandler(opt, Icon)}
+                >
+                    {!Icon ? label : <Icon size={25} />}
                 </button>
             </div>
-        )
+        );
     }
 }
 
-export default CalculatorOpt
+export default CalculatorOpt;
