65035c9ed0d63c8e15615ac4782e44f1d0aaea15
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 15:23:08 -0300

    refactor: changed styles

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -13,14 +13,13 @@ class HistoryContent extends Component<HistoryContentProps> {
         const { history } = this.props;
 
         return (
-            <div className="box-border flex flex-col justify-between items-center text-center text-gray-200 h-full w-[40%]">
+            <div className="box-border hidden md:flex flex-col justify-between items-center text-center text-gray-200 h-full w-[40%]">
                 <div className="box-border flex justify-center items-center p-1 h-[5%] w-full shadow-customC bg-customColorB">
                     <label className="relative truncate text-sm font-bold font-sans h-fit w-full">
                         Calculation History
                     </label>
                 </div>
-                <div className="box-border hidden md:flex gap-2 flex-col justify-start items-center p-6 h-[85%] w-full
-                overflow-hidden overflow-y-auto">
+                <div className="box-border flex gap-2 flex-col justify-start items-center p-6 h-[85%] w-full overflow-hidden overflow-y-auto">
                     <div className="flex gap-2 flex-col justify-start items-center h-fit w-full">
                         {history.map((item, index) => {
                             const { expression, result } = item;
