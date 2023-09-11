bae399aedc27e8d915e2695428e411ebec37d15c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 15:13:44 -0300

    feat: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Component, ReactNode } from "react"
+
+
+class Footer extends Component {
+    render(): ReactNode {
+        return (
+            <footer>
+                footer
+            </footer>
+        )
+    }
+}
+
+export default Footer
