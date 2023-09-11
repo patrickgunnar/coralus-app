a09405306533fac4e006717a51979de2a8529bd0
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 17:36:02 -0300

    feat: error page

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ErrorLayout from "@/components/ErrorLayout";
+import { Component, ReactNode } from "react";
+
+
+class Error extends Component {
+    render(): ReactNode {
+        return <ErrorLayout />;
+    }
+}
+
+export default Error;
