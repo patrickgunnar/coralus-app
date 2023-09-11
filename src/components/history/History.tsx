e544581ae6362737e507494b677f3e48a732e7e0
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 15:13:58 -0300

    feat: added styles and imported footer

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -3,6 +3,7 @@
 import useHistory, { HistoryType } from "@/hooks/useHistory";
 import { Component, ReactNode } from "react";
 import HistoryItem from "./HistoryItem";
+import Footer from "../Footer";
 
 interface HistoryContentProps {
     history: HistoryType[];
@@ -12,11 +13,16 @@ class HistoryContent extends Component<HistoryContentProps> {
         const { history } = this.props;
 
         return (
-            <div className="box-border hidden md:flex gap-2 flex-col justify-start items-center p-6 h-full w-[40%]
-            overflow-hidden overflow-y-auto">
-                <div className="flex gap-2 flex-col justify-start items-center h-fit w-full">
-                    {history.map(
-                        (item, index) => {
+            <div className="box-border flex flex-col justify-between items-center text-center text-gray-200 h-full w-[40%]">
+                <div className="box-border flex justify-center items-center p-1 h-[5%] w-full shadow-customC bg-customColorB">
+                    <label className="relative truncate text-sm font-bold font-sans h-fit w-full">
+                        Calculation History
+                    </label>
+                </div>
+                <div className="box-border hidden md:flex gap-2 flex-col justify-start items-center p-6 h-[85%] w-full
+                overflow-hidden overflow-y-auto">
+                    <div className="flex gap-2 flex-col justify-start items-center h-fit w-full">
+                        {history.map((item, index) => {
                             const { expression, result } = item;
 
                             return (
@@ -26,8 +32,11 @@ class HistoryContent extends Component<HistoryContentProps> {
                                     result={result}
                                 />
                             );
-                        }
-                    )}
+                        })}
+                    </div>
+                </div>
+                <div className="box-border flex justify-center items-center p-1 h-[5%] w-full shadow-customC bg-customColorB">
+                    <Footer />
                 </div>
             </div>
         );
