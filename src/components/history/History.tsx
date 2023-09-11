3af515a8846dbf36152559b1c93aaa0f6a7e611d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 14:02:14 -0300

    feat: added class wrapper

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -1,16 +1,28 @@
-"use client"
+"use client";
 
+import useHistory, { HistoryType } from "@/hooks/useHistory";
 import { Component, ReactNode } from "react";
 
-
-class History extends Component {
+interface HistoryContentProps {
+    history: HistoryType[];
+}
+class HistoryContent extends Component<HistoryContentProps> {
     render(): ReactNode {
+        const { history } = this.props;
+        console.log(history);
+
         return (
             <div className="box-border hidden md:flex justify-center items-center h-full w-[40%]">
                 <label>Calculator History</label>
             </div>
-        )
+        );
     }
 }
+
+const History = () => {
+    const { history } = useHistory();
+
+    return <HistoryContent history={history} />;
+};
 
 export default History;
