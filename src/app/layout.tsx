9a58cce940470c5a8402ee02e1b6297acb691108
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 17:32:40 -0300

    feat: added footer to mobile screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,10 @@ export default function RootLayout({
             <body className={`${inter.className} box-border relative flex justify-center items-center h-full w-full bg-customSize bg-customLinearGradient`}>
 				{children}
                 <About />
+                <div className="box-border absolute flex md:hidden justify-center items-center h-[1.95rem] w-full bottom-0 
+                text-white bg-[var(--colour-009)]">
+                    <Footer />
+                </div>
 			</body>
         </html>
     );
