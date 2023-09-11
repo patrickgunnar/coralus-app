66de126a879c509e964096f1bac248cb6e5e72cf
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 17:23:53 -0300

    feat: added page reload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ class Header extends Component {
             <header className="box-border flex justify-center items-center h-fit w-fit">
                 <button className="box-border flex gap-1 justify-center items-center p-2 mt-4 
                 h-fit w-fit uppercase font-bold text-[1.25rem] text-[var(--colour-008)] 
-                text-center font-header">
+                text-center font-header" onClick={() => window.location.reload()}>
                     <MdCalculate size={22} className="mb-1" />
                     Coralus
                 </button>
