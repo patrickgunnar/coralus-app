fda0176427b8b949486f926e9ecbe39a2a91bd8e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 17:36:15 -0300

    feat: loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import { Component, ReactNode } from "react";
+
+
+class Loading extends Component {
+    render(): ReactNode {
+        return <Loader />;
+    }
+}
+
+export default Loading;
