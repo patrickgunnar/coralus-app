8a96e4f0d9ac5134f4be6d1ee9957e375a7539f9
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-11 16:55:44 -0300

    feat: added class wrapper

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,45 @@
-"use client"
+"use client";
 
-import { Component, ReactNode } from "react"
+import useAboutModal from "@/hooks/useAboutModal";
+import { Component, ReactNode } from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
 
+interface FooterProps {
+    setIsOpened: () => void;
+}
 
-class Footer extends Component {
+class FooterContent extends Component<FooterProps> {
+    constructor(props: FooterProps) {
+        super(props);
+    }
+
     render(): ReactNode {
+        const { setIsOpened } = this.props;
+
         return (
-            <footer>
-                footer
+            <footer className="box-border flex gap-6 justify-between items-center p-1 h-full w-full text-[0.7rem] font-semibold uppercase">
+                <a
+                    className="cursor-pointer font-sans"
+                    href="https://patrickgunnar.vercel.app/"
+                    target="_blank"
+                >
+                    © Patrick Gunnar - All Rights Reserved
+                </a>
+                <button
+                    className="box-border flex justify-center items-center h-fit w-fit"
+                    onClick={setIsOpened}
+                >
+                    <BsInfoCircleFill size={22} />
+                </button>
             </footer>
-        )
+        );
     }
 }
 
-export default Footer
+const Footer = () => {
+    const { setIsOpened } = useAboutModal()
+
+    return <FooterContent setIsOpened={setIsOpened} />;
+};
+
+export default Footer;
